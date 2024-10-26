15c8cc0ed8479207f102c218089eda9448221ccd
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-10-27 02:47:30 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -67,7 +67,7 @@ LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
 
 # Your User ID.
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "7009601543").split())
+    map(int, getenv("OWNER_ID", "6678232579").split())
 )  # Input type must be interger
 
 
